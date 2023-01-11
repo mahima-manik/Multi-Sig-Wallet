1b14728e630226cfb80eafdd363784ab3f1f282b
Mahima Manik <mahimamanik@Mahimas-MacBook-Pro-2.local>
2023-01-11 20:23:38 +0530

    feat: initializing hardhat project

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
+require("@nomiclabs/hardhat-ethers");
+
+const ALCHEMY_API_KEY = "";
+
+const GOERLI_PRIVATE_KEY = "";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
+};
